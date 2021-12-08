c7e0227c707d88f21ef4e4fac068b0fd541aa820
Roushan <roushan1990@gmail.com>
2021-12-08 13:12:31 +0530

    Add project files.

---
+++ StringDistinct/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace StringDistinct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.WriteLine("Enter String1:");
+            string str1 = Console.ReadLine();
+            Console.WriteLine("Enter String2:");
+            string str2 = Console.ReadLine();
+            var op1 = str1.DistinctFrom(str2);
+            var op2 = str2.DistinctFrom(str1);
+            Console.WriteLine("Op1:" + op1);
+            Console.WriteLine("Op2:" + op2);
+        }
+    }
+    
+}
---
+++ StringDistinct/StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringDistinct
+{
+    public static class StringExtensions
+    {
+        public static string DistinctFrom(this string one, string two)
+        {
+            return new string(one.Except(two).ToArray());
+        }
+    }
+}
